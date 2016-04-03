d1e59685c88f532c3a9488bd71c08c229468111c
MingMing <cmm.thu@gmail.com>
2016-04-03 11:46:15 +0800

    Update to be compatible with OpenCV 3.x

--- CmLib/CmLib.h
+++ CmLib/CmLib.h
@@ -42,10 +42,15 @@ using namespace std;
 #define CV_VERSION_ID CVAUX_STR(CV_MAJOR_VERSION) CVAUX_STR(CV_MINOR_VERSION) CVAUX_STR(CV_SUBMINOR_VERSION)
 #define cvLIB(name) lnkLIB("opencv_" name CV_VERSION_ID)
 
+#if CV_MAJOR_VERSION == 3
+#pragma comment(lib, cvLIB("world"))
+#else
 #pragma comment( lib, cvLIB("core"))
 #pragma comment( lib, cvLIB("imgproc"))
 #pragma comment( lib, cvLIB("highgui"))
 #pragma comment(lib, cvLIB("contrib"))
+#endif
+
 using namespace cv;
 
 
